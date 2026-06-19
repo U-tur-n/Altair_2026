15de56b218d8976d59cc617a27bb2202c4af4aab
YutaARAKAWA_dynabook <a.yuuta0413@gmail.com>
2026-06-19 11:16:18 +0900

    Revert "BMEの関数追加"
    This reverts commit 47afe3a46d5906048473e5eebca7eefcb348e3da.

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,10 @@
 
 void setup() {
   // put your setup code here, to run once:
-  BME::begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  BME::execute();
 }
 
 // put function definitions here:
