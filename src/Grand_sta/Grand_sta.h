4113d2482a468b59bc0a30c942d3ffcee6cff2fe
Yuta ARAKAWA <a.yuuta0413@gmail.com>
2026-06-28 01:53:00 +0900

    Create Grand_sta.h

---
+++ src/Grand_sta/Grand_sta.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Arduino.h>
+
+namespace Grand_sta {
+    extern float ax;
+    extern float ay;
+    extern float az;
+    void begin();
+    void extern();
+    // extern float roll;
+    // extern float pitch;
+    // extern float yaw;
+}
