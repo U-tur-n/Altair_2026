e64f657eda31467a84d498a28563e759c7d16b24
Yuta ARAKAWA <a.yuuta0413@gmail.com>
2026-06-27 21:17:12 +0900

    Wire関連追加

--- src/BNO/BNO.cpp
+++ src/BNO/BNO.cpp
@@ -15,6 +15,9 @@ float q;
 
 void begin(){
   Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(50000);   // 通信速度を50kHzに下げて安定させる
+  Wire.setTimeOut(20000); // センサーの演算待ちによるタイムアウトを防ぐ
 
   if (!bno.begin()) {
     Serial.println("no BNO055 detected");
