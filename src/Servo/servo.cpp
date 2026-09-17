1cb5ad24197f0354fe5ae34638636cc7739f3ecb
Yuta ARAKAWA <a.yuuta0413@gmail.com>
2026-09-17 19:29:33 +0900

    begin関数に固定位置まで回転するコードを追加

--- src/Servo/servo.cpp
+++ src/Servo/servo.cpp
@@ -7,6 +7,19 @@ namespace servo{
   void begin() {
    pinMode(servoPin, OUTPUT);
    Serial.begin(9600);
+   // 終点位置(501)から90度の位置(約1450)まで回転
+       for (DutyCycle = 501; DutyCycle <= 1450; DutyCycle += 5) {
+          digitalWrite(servoPin, HIGH);
+          delayMicroseconds(DutyCycle);
+          digitalWrite(servoPin, LOW);
+          delayMicroseconds(20000 - DutyCycle);
+       }
+
+       // 90度の位置で保持（戻らないように）
+       digitalWrite(servoPin, HIGH);
+       delayMicroseconds(1450);
+       digitalWrite(servoPin, LOW);
+       delayMicroseconds(20000 - 1450);
   }
 
   void execute() {
