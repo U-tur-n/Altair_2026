0e49a794e7269e58e35bdfc7f2cb78417137c757
Yuta ARAKAWA <a.yuuta0413@gmail.com>
2026-09-22 00:39:54 +0900

    演算待ちタイムアウト時間変更

--- src/BNO/BNO.cpp
+++ src/BNO/BNO.cpp
@@ -17,7 +17,7 @@ bool BNObegin(){
   // Serial.begin(115200);
   Wire.begin();
   Wire.setClock(50000);   // 通信速度を50kHzに下げて安定させる
-  Wire.setTimeOut(20000); // センサーの演算待ちによるタイムアウトを防ぐ
+  Wire.setTimeOut(50); // センサーの演算待ちによるタイムアウトを防ぐ
 
   if (!bno.begin()) {
     Serial.println("no BNO055 detected");
